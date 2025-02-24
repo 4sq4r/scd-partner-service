f4bc5454508000a2b76e38f0590a7eb1b090cbdd
Askar.A <aalyiev7@gmail.com>
2025-02-24 14:33:19 +0500

    second commit

---
+++ src/main/java/kz/demo/model/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package kz.demo.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+
+        if (this.createdAt == null) {
+            this.createdAt = now;
+            this.updatedAt = now;
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}

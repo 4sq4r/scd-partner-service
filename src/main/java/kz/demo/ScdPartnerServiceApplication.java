7ba93b0701b2131e31844df8c931a389bb6dff25
Askar.A <aalyiev7@gmail.com>
2025-02-24 14:13:59 +0500

    first commit

---
+++ src/main/java/kz/demo/ScdPartnerServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdPartnerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScdPartnerServiceApplication.class, args);
+    }
+
+}

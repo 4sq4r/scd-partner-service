1370846b947456b735621546e96dd2c948b82f4d
Askar.A <aalyiev7@gmail.com>
2025-03-26 15:34:22 +0500

    commented contextLoads

--- src/test/java/kz/partnerservice/PartnerServiceApplicationTests.java
+++ src/test/java/kz/partnerservice/PartnerServiceApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PartnerServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 }

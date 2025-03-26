c91ed112bc1f080e54a43799363036cd2fcbf871
Askar.A <aalyiev7@gmail.com>
2025-03-26 15:31:34 +0500

    init

---
+++ src/test/java/kz/partnerservice/PartnerServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package kz.partnerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
